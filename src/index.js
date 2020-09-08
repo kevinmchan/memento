27cd158104d2048aea05e5c32b7745735aafa438
Kevin Chan <kevinmchan@live.com>
2020-09-08 14:12:20 -0400

    Add navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const relationship = 'Michelle & Kevin'
+
+const Nav = ({ brand }) => (
+    <nav className="uk-navbar-container" data-uk-navbar>
+        <div className="uk-navbar-left">
+            <ul className="uk-navbar-nav">
+                <li className="uk-active"><a href="#">Memento</a></li>
+            </ul>
+        </div>
+        <div className="uk-navbar-center">
+            <h3 className="uk-margin-remove">{brand}</h3>
+        </div>
+        <div className="uk-navbar-right">
+            <ul className="uk-navbar-nav">
+                <li className="uk-active"><a href="#"><span uk-icon="user"></span></a></li>
+            </ul>
+        </div>
+    </nav>
+);
 
 const App = () => (
-    <div>
-        Hello World!
-    </div>
-)
+    <>
+        <Nav brand={relationship} />
+    </>
+);
 
 ReactDOM.render(
     <App />,
