985bc2555d6f6a411dbfc7937673bbf2d000cb88
Kevin Chan <kevinmchan@live.com>
2020-09-08 13:53:01 -0400

    Initial express server setup

---
+++ server.js
@@ -0,0 +1,34 @@
+import config from './config';
+import apiRouter from './api';
+
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
+import express from 'express';
+
+/* create server object */
+const server = express();
+
+/* use sass middleware to convert sass to css */
+server.use(sassMiddleware({
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public')
+}));
+
+/* use ejs to render index file */
+server.set('view engine', 'ejs');
+
+/* make api router accessible */
+server.use('/api', apiRouter);
+
+/* make dest public folder accessible */
+server.use(express.static('public'));
+
+/* route root to index file */
+server.get('/', (req, res) => {
+  res.render('index');
+});
+
+/* listen on configured port */
+server.listen(config.port, config.host, () => {
+  console.info('Express listening on port', config.port);
+});
