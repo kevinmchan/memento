20345e69b16472e7bc645d67ffe680c5d75b7f8c
Kevin Chan <kevinmchan@live.com>
2020-09-19 16:27:47 -0400

    Connect dummy hooks to new event form

--- src/component/newevent.js
+++ src/component/newevent.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const NewEvent = ({ activeDate }) => {
+  const [eventDate, setEventDate] = useState(activeDate);
+  const [eventName, setEventName] = useState("");
+  const [eventDesc, setEventDesc] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  useEffect(() => {
+    setEventDate(activeDate);
+  }, [activeDate]);
+
   return (
     <div className="uk-section-xsmall">
       <div className="uk-container uk-width-2xlarge">
@@ -24,13 +33,16 @@ const NewEvent = ({ activeDate }) => {
             />
             <form className="uk-margin">
               <fieldset className="uk-fieldset">
-                <legend className="uk-legend">New memento</legend>
+                <legend className="uk-legend">
+                  {eventName ? "New: " + eventName : "New memento"}
+                </legend>
 
                 <div className="uk-margin">
                   <input
                     className="uk-input"
                     type="text"
                     placeholder="Event name"
+                    onChange={(event) => setEventName(event.target.value)}
                   />
                 </div>
 
@@ -39,36 +51,43 @@ const NewEvent = ({ activeDate }) => {
                     className="uk-textarea"
                     rows="5"
                     placeholder="Event description"
+                    onChange={(event) => setEventDesc(event.target.value)}
                   />
                 </div>
 
                 <div className="uk-margin">
                   <input
-                    type="text"
-                    data-role="calendarpicker"
-                    data-dialog-mode="true"
-                    data-input-format="%y-%m-%d"
-                    defaultValue={activeDate.toISOString().substring(0, 10)}
+                    type="date"
+                    value={eventDate.toISOString().substring(0, 10)}
+                    onChange={(event) =>
+                      setEventDate(new Date(event.target.value))
+                    }
                   />
                 </div>
 
                 <div className="js-upload" data-uk-form-custom>
-                  <input type="file" />
+                  <input
+                    type="file"
+                    onChange={(event) => {
+                      setSelectedFile(event.target.files[0]);
+                    }}
+                  />
                   <button className="uk-button uk-button-default" type="button">
-                    Upload photo
+                    {selectedFile ? "Change Photo" : "Select photo"}
                   </button>
                 </div>
               </fieldset>
             </form>
-            <p className="uk-text-right">
+            <div className="uk-text-right">
               <button
                 className="uk-button uk-button-primary"
                 type="button"
                 data-uk-toggle="target: .new-event-toggle; cls: uk-hidden"
+                onClick={() => {}}
               >
                 Create
               </button>
-            </p>
+            </div>
           </div>
         </div>
       </div>
