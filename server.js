636baf6d424cf91b9ecd89ed773c4b7df1132062
Kevin Chan <kevinmchan@live.com>
2020-10-01 23:39:37 -0400

    Remove sass file not being used

--- server.js
+++ server.js
@@ -13,14 +13,6 @@ server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 
-/* use sass middleware to convert sass to css */
-server.use(
-  sassMiddleware({
-    src: path.join(__dirname, "sass"),
-    dest: path.join(__dirname, "public"),
-  })
-);
-
 /* use ejs to render index file */
 server.set("view engine", "ejs");
 
